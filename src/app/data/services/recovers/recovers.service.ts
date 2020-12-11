76120e0cf0d5101a51d38341af7e81168de80c9d
JBravo <bravo.jose.luis18@gmail.com>
2020-12-11 12:36:19 -0300

    add change header

--- src/app/data/services/recovers/recovers.service.ts
+++ src/app/data/services/recovers/recovers.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
 
 const headers = new HttpHeaders({
   'Content-Type': 'application/json, text/plain, */*',
-  'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJBcHBsaWNhdGlvbklkIjoiMSIsIkFwcGxpY2F0aW9uVG9rZW4iOiJmZTgwNGUzOC1iMTA0LTQyNDEtOWE1My1mYWY5NmE0ZjIyMzYiLCJBdXRvbWF0aW9uQWNjZXNzIjoiVHJ1ZSIsIm5hbWVpZCI6IjQ1YmRjOTYwLTU3ZjctNDU3Mi1iNTM4LTk5OTRkNTJlOTAzYiIsInVuaXF1ZV9uYW1lIjoiSkJyYXZvQGFzb2NpYXJ0LmNvbS5hciIsImVtYWlsIjoiSkJyYXZvQGFzb2NpYXJ0LmNvbS5hciIsIkZpcnN0TmFtZSI6Ikpvc8ODwqkgTHVpcyIsIkxhc3ROYW1lIjoiQnJhdm8iLCJUYXhJZCI6IiIsIk9mZmljaWFsSWRUeXBlIjoiIiwiZ2VuZGVyIjoiIiwiQ3VpdEN1aWwiOiIiLCJyb2xlIjpbIkFkbWluaXN0cmFkb3IiLCJVc3VhcmlvIl0sIm5iZiI6MTYwNzY5NDc0NiwiZXhwIjoxNjA3NzAxOTQ2LCJpYXQiOjE2MDc2OTQ3NDZ9.EbLBX3bdTfIa4d5NO7Yy3EuAgwIyiP4KLnBd86NY_aI`
+  'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJBcHBsaWNhdGlvbklkIjoiMSIsIkFwcGxpY2F0aW9uVG9rZW4iOiJmZTgwNGUzOC1iMTA0LTQyNDEtOWE1My1mYWY5NmE0ZjIyMzYiLCJBdXRvbWF0aW9uQWNjZXNzIjoiVHJ1ZSIsIm5hbWVpZCI6IjQ1YmRjOTYwLTU3ZjctNDU3Mi1iNTM4LTk5OTRkNTJlOTAzYiIsInVuaXF1ZV9uYW1lIjoiSkJyYXZvQGFzb2NpYXJ0LmNvbS5hciIsImVtYWlsIjoiSkJyYXZvQGFzb2NpYXJ0LmNvbS5hciIsIkZpcnN0TmFtZSI6Ikpvc8ODwqkgTHVpcyIsIkxhc3ROYW1lIjoiQnJhdm8iLCJUYXhJZCI6IiIsIk9mZmljaWFsSWRUeXBlIjoiIiwiZ2VuZGVyIjoiIiwiQ3VpdEN1aWwiOiIiLCJyb2xlIjpbIkFkbWluaXN0cmFkb3IiLCJVc3VhcmlvIl0sIm5iZiI6MTYwNzcwMDM0NCwiZXhwIjoxNjA3NzA3NTQ0LCJpYXQiOjE2MDc3MDAzNDR9.G6aYoOOy6IvuXZoRifSL1rcXdDJWKzRwJT9hiG07x6g`
 })
 
 @Injectable({
@@ -18,12 +18,12 @@ export class RecoversService {
 
   closeRecoverDifferenceValuesAsync(request: any) 
   {
-    return this.http.post(environment.recoverUrl + recoversRoutes.home.closeRecover, {headers: headers}, request)
+    return this.http.post(environment.recoverUrl + recoversRoutes.home.closeRecover, request, {headers: headers})
   }
 
   rejectClosingRecoverAsync(request: any) 
   {
-    return this.http.post(environment.recoverUrl + recoversRoutes.home.rejectRecover, {headers: headers}, request)
+    return this.http.post(environment.recoverUrl + recoversRoutes.home.rejectRecover, request, {headers: headers})
   }
 
   readClosingReasonsDifferenceAsync(): Observable<any> {
