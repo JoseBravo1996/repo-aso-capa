017c6b846228bc9824b24587e1ae24f3a218bc1d
JBravo <bravo.jose.luis18@gmail.com>
2020-12-18 12:22:58 -0300

    unsuscribe

--- src/app/modules/back-to-the-future/doc-brown/doc-brown.component.ts
+++ src/app/modules/back-to-the-future/doc-brown/doc-brown.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { BackToTheFutureService } from '@data/services/back-to-the-future/back-to-the-future.service';
+import { add } from 'ngx-bootstrap/chronos';
+import { Subscription } from 'rxjs';
 import { take } from 'rxjs/operators';
 
 @Component({
@@ -8,16 +10,23 @@ import { take } from 'rxjs/operators';
   templateUrl: './doc-brown.component.html',
   styleUrls: ['./doc-brown.component.scss']
 })
-export class DocBrownComponent implements OnInit {
+export class DocBrownComponent implements OnInit, OnDestroy {
 
   messageToMarty: string;
   form: FormGroup;
+  private suscription: Subscription;
+  private suscription$: Subscription[] = [];
 
   constructor(private formBuilder: FormBuilder, private backToTheFutureService: BackToTheFutureService) {
     this.backToTheFutureService.telegrafista$.subscribe(
-      resp => console.log(resp)
     )
    }
+
+  ngOnDestroy(): void {
+    this.suscription.unsubscribe();
+    this.suscription$.forEach((sub) => sub.unsubscribe());
+
+  }
 
   ngOnInit() {
     this.form = this.formBuilder.group({
@@ -31,8 +40,12 @@ export class DocBrownComponent implements OnInit {
   }
 
   viewMessage() {
-    this.backToTheFutureService.telegrafista$.pipe(take(1)).
+    this.suscription = this.backToTheFutureService.telegrafista$.pipe(take(1)).
       subscribe(mensaje => this.messageToMarty = mensaje);
+
+      this.suscription.add(this.backToTheFutureService.telegrafista$.pipe(take(1)).
+      subscribe(mensaje => this.messageToMarty = mensaje));
   }
+
 
 }
--- src/app/modules/recovers/recovers-list/recovers-list/recovers-list.component.ts
+++ src/app/modules/recovers/recovers-list/recovers-list/recovers-list.component.ts
@@ -1,3 +1,4 @@
+import { BackToTheFutureService } from './../../../../data/services/back-to-the-future/back-to-the-future.service';
 import { ReadClosingReasonsDifference } from '@data/interfaces/recover/readClosingReasonsDifference';
 import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
@@ -5,6 +6,7 @@ import { ScCurrencyPipe } from '@sc/portal.fe.lib.ui-core-components';
 import { CloseRecoverDifferenceValues, RejectClosingRecover } from '@data/interfaces/recover';
 import { RecoversService } from '@data/services/recovers/recovers.service';
 import { ToastrService } from 'ngx-toastr';
+import { filter, map, take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-recovers-list',
@@ -19,13 +21,14 @@ export class RecoversListComponent implements OnInit {
   public reasons: ReadClosingReasonsDifference[] = [];
 
   constructor(private formBuilder: FormBuilder, private scCurrency: ScCurrencyPipe,
-    private recoversService: RecoversService, private toastrService: ToastrService) { 
+    private recoversService: RecoversService, private toastrService: ToastrService,private backToTheFutureService: BackToTheFutureService) {
       this.recoverId = 21;
     }
 
   ngOnInit() {
     this.createForm();
     this.readClosinReason();
+    this.viewMessage();
   }
 
   createForm() {
@@ -72,6 +75,15 @@ export class RecoversListComponent implements OnInit {
     this.recoversService.readClosingReasonsDifferenceAsync().subscribe(resp => {
       this.reasons = resp.Reasons;
     });
+  }
+
+  viewMessage() {
+    const valor = this.backToTheFutureService.telegrafista$.pipe(
+      filter(char => char == 'hola'),
+      map(char => char.toUpperCase()),
+    );
+
+      valor.subscribe(mensaje => console.log(mensaje));
   }
 
 }
